397c26634bdfe95d280fdee7d06deb8b21fb4ca4
qazBK <kbolyarskikh@gmail.com>
2024-10-02 19:56:19 +0500

    у
    у

---
+++ example-app/resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+@for ($i=0; $i < 10; $i++)
+    The current Valuue is {{i}}<br />
+
+</html>
