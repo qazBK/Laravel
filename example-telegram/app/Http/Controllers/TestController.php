ced2c87460fe3c9b7e5e87a82e5578d92e883e76
qazBK <kbolyarskikh@gmail.com>
2025-02-19 18:54:15 +0500

    л7щл

--- example-telegram/app/Http/Controllers/TestController.php
+++ example-telegram/app/Http/Controllers/TestController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DefStudio\Telegraph\Models\TelegraphChat;
 
 class TestController extends Controller
 {
-    //
+    public function index() {
+
+        //        use DefStudio\Telegraph\Models\TelegraphChat;
+        $chat = TelegraphChat::find(1); //id первого чата в базе данных 
+        $chat->message('hello')->send();
+        $chat->html("<b>hello</b>\n\nI'm a bot!")->send();
+        $chat->markdown('*hello*')->send();        
+        return "test-OK";
+    }    
 }
