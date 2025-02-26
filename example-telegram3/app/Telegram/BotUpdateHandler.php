488ee35324ef64ba63fe391307dfcf6bd0ef8754
qazBK <kbolyarskikh@gmail.com>
2025-02-26 18:30:34 +0500

    н
    н

---
+++ example-telegram3/app/Telegram/BotUpdateHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Telegram;
+
+use WeStacks\TeleBot\Handlers\UpdateHandler;
+use WeStacks\TeleBot\Objects\Update;
+use WeStacks\TeleBot\TeleBot;
+
+class BotUpdateHandler extends UpdateHandler
+{
+    public function trigger(): bool
+    {
+        //Когда нужно обрабатывать...
+        return isset($this->update->message); // handle regular messages (example)
+    }
+
+    public function handle()
+    {
+        $update = $this->update;
+        $bot = $this->bot;
+
+        // chat_id => $this->update->message->chat->id
+        $data = $this->update->message()->text;
+
+        return $this->sendMessage([
+            'text' => 'Вы написали: ' . $data,
+            'reply_markup' => [
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'Google',
+                        'url' => 'https://google.com/'
+                        ],
+                        ['text' => 'Удалить 6',
+                        'callback_data' => 'test:delete:6'
+                        ],
+                    ]
+                    ]
+                ]   
+
+        ]);
+    }
+}
